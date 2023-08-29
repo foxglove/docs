46b31449e4e265091ef5a7f580b74b076c171cd4
Esther S. Weon <esther.s.weon@gmail.com>
2023-08-29 16:49:27 -0700

    Placeholder for home page (#30)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,27 @@ import React, { ReactElement } from "react";
 export default function index(): ReactElement {
   return (
     <Layout>
-      <div style={{ padding: "5em", textAlign: "center" }}>Docs home</div>
+      <div style={{ textAlign: "center" }}>
+        <img
+          alt="Foxglove logo"
+          src="/img/full-logo.png"
+          style={{ padding: "5em", paddingBottom: "1em", width: "70vw" }}
+        />
+        <h2 style={{ fontSize: "2rem" }}>
+          The leading observability platform for robotics developers
+        </h2>
+        <div style={{ marginTop: "5em", display: "flex", justifyContent: "center" }}>
+          <a
+            href="/docs/introduction/why-foxglove"
+            style={{ textDecoration: "underline", marginRight: "20px" }}
+          >
+            View Docs
+          </a>
+          <a href="https://foxglove.dev/docs/api" style={{ textDecoration: "underline" }}>
+            View API Reference
+          </a>
+        </div>
+      </div>
     </Layout>
   );
 }
