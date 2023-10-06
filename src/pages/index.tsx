1f28a1ac3e8d4bb27973893bc9dab642620f78b9
Esther Weon <esther.s.weon@gmail.com>
2023-10-05 23:59:50 -0700

    Add React router redirect from landing page to docs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,6 @@
-import useBaseUrl from "@docusaurus/useBaseUrl";
-import Layout from "@theme/Layout";
-import React, { ReactElement, useEffect } from "react";
+import React, { ReactElement } from "react";
+import { Navigate } from "react-router-dom";
 
 export default function index(): ReactElement {
-  useEffect(() => {
-    window.location.href = useBaseUrl("/docs/main");
-  }, []);
-
-  return (
-    <Layout>
-      <div style={{ textAlign: "center" }}>
-        <img
-          alt="Foxglove logo"
-          src="/img/full-logo.png"
-          style={{ padding: "5em", paddingBottom: "1em", width: "70vw" }}
-        />
-        <h2 style={{ fontSize: "2rem" }}>
-          The leading observability platform for robotics developers
-        </h2>
-        <div style={{ marginTop: "5em", display: "flex", justifyContent: "center" }}>
-          <a
-            href="/docs/introduction/why-foxglove"
-            style={{ textDecoration: "underline", marginRight: "20px" }}
-          >
-            View Docs
-          </a>
-          <a href="/api" style={{ textDecoration: "underline" }}>
-            View API Reference
-          </a>
-        </div>
-      </div>
-    </Layout>
-  );
+  return <Navigate to="/docs/introduction/why-foxglove" />;
 }
