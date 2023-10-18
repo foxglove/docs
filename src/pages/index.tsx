dfc7f6334ad224269384882a9eb3c25b7e52c374
Esther Weon <esther.s.weon@gmail.com>
2023-10-18 12:47:43 -0700

    Update redirect URL

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,5 @@ import React, { ReactElement } from "react";
 import { Navigate } from "react-router-dom";
 
 export default function index(): ReactElement {
-  return <Navigate to="/docs/introduction/why-foxglove" />;
+  return <Navigate to="/docs/introduction" />;
 }
