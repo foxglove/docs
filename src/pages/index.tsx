53d91d398cca1516dcfe450ed3b967b6de5b0b2a
Esther Weon <esther.s.weon@gmail.com>
2023-10-18 12:49:10 -0700

    Fix error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from "react";
-import { Navigate } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
 export default function index(): ReactElement {
-  return <Navigate to="/docs/introduction" />;
+  return <Redirect to="/docs/introduction" />;
 }
