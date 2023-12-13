daacfe5e13b05fed161e257f13480b36ac726058
Esther Weon <esther.s.weon@gmail.com>
2023-12-12 19:07:09 -0800

    Add Inter var font

--- docusaurusConfig.ts
+++ docusaurusConfig.ts
@@ -32,21 +32,7 @@ const docusaurusConfig: Config = {
     defaultLocale: "en",
     locales: ["en"],
   },
-  headTags: [
-    // Add Google Font version of Inter
-    { tagName: "link", attributes: { rel: "preconnect", href: "https://fonts.googleapis.com" } },
-    {
-      tagName: "link",
-      attributes: { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "true" },
-    },
-    {
-      tagName: "link",
-      attributes: {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap",
-      },
-    },
-  ],
+  headTags: [],
 
   presets: [
     [
