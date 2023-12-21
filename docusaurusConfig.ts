b9b13ffd23874cd0b6a64b1de7fd0d4871300f13
Esther Weon <esther.s.weon@gmail.com>
2023-12-21 11:41:40 -0800

    Fix prod URL in docusauruas project

--- docusaurusConfig.ts
+++ docusaurusConfig.ts
@@ -17,7 +17,7 @@ const docusaurusConfig: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://foxglove.dev",
+  url: "https://docs.foxglove.dev",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
